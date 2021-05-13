f26092700f44b65d1e89396ffee29dd5b839a7c3
avinahuja <avin@ahuja.ws>
2021-05-13 16:05:52 +0530

    y

--- Chain.js
+++ Chain.js
@@ -1,20 +1,27 @@
-class Chain{
-    constructor(bodyA, bodyB){
+class slingshot{
+    constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
-            bodyB: bodyB,
+            pointB: pointB,
             stiffness: 0.04,
             length: 10
         }
-        this.chain = Constraint.create(options);
-        World.add(world, this.chain);
+        this.sling = Constraint.create(options);
+        this.pointB = pointB
+        World.add(world, this.sling);
+    }
+    fly(){
+        this.sling.bodyA = null;
     }
 
     display(){
-        var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
-        strokeWeight(4);
-        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+        
     }
     
 }
